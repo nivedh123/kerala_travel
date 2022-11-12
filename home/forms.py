56377df46b399ad1837fb52db7a81088296ce882
nivedh123 <nivvkp12@gmail.com>
2022-11-12 18:19:55 +0530

    front end start

--- home/forms.py
+++ home/forms.py
@@ -2,6 +2,7 @@
 from django import forms
 from .models import spot,reviewmodel,profilemodel
 from django import forms  
+from django.forms import Textarea
 from django.contrib.auth.models import User  
 from django.contrib.auth.forms import UserCreationForm  
 from django.core.exceptions import ValidationError  
@@ -24,19 +25,25 @@ class UserCreationForm(UserCreationForm):
 class profileform(forms.ModelForm):
     class Meta:
         model=profilemodel
-        fields=['Image','Name','Bio']
+        fields=['Image','Name','Bio_head','Bio','link']
+        labels={'Image':'profile Photo','Name':'Profile name','Bio_head':'title of your bio',
+                'Bio':'bio','link':'Social media profile link'}
  
 class searchFormbyDistrict(forms.Form):
-    district=forms.CharField(max_length=20)
+    district=forms.CharField(label='',widget=forms.TextInput,max_length=50)
+
 
 class reviewForm(forms.ModelForm):
     class Meta:
         model=reviewmodel
-        fields=['content','rating']
+        fields=['content','rating','status']
+        labels={'content':'write here','status':'did you visited the spot'}
 class spotform(forms.ModelForm):
     class Meta:
         model=spot
-        fields=['image','image2','image3','name','discription','link','district','type','warning']
-   
+        fields=['image','image2','image3','name','short_discription','discription','key_words','rel_link','link','district','type','warning']
+        labels={'name':'Name of spot','short_discription':'discription title','discription':'discription',
+                'key_words':'key words','rel_link':'referance','link':'location url'}
+        widgets={'discription':Textarea(attrs={'cols': 80, 'rows': 3}),'warning':Textarea(attrs={'cols': 80, 'rows': 3}),}
 
    
--- home/models.py
+++ home/models.py
@@ -9,11 +9,18 @@ from django.core.files import File
 #image compression method
 def compress(image):
     im = Image.open(image)
+    im = im.resize((1920 ,1440), Image.ANTIALIAS)
     im_io = BytesIO() 
     im.save(im_io, 'JPEG', quality=60) 
     new_image = File(im_io, name=image.name)
     return new_image
-
+def Compress(image):
+    im = Image.open(image)
+    im = im.resize((400,400), Image.ANTIALIAS)
+    im_io = BytesIO() 
+    im.save(im_io, 'JPEG', quality=60) 
+    new_image = File(im_io, name=image.name)
+    return new_image
 
 
 # Create your models here.
@@ -37,35 +44,43 @@ class remark(models.Model):
         return self.alert
 
 class profilemodel(models.Model):
-    Image=models.ImageField('profile/')
+    Image=models.ImageField()
+    Bio_head=models.CharField(max_length=40,help_text='like PROFFESSIONAL PHOTOGRAPHER FOR 4 YEARS')
     Name=models.CharField(max_length=20)
     Bio=models.TextField()
     Date=models.DateTimeField(auto_now_add=True)
+    link=models.URLField()
     user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='profilemodel')
     verify=models.BooleanField(default=False)
     def save(self, *args, **kwargs):
-        new_image = compress(self.Image)
+        new_image = Compress(self.Image)
         self.Image = new_image
         super().save(*args, **kwargs)
     def __str__(self):
         return str(self.user)
 
 class spot(models.Model):
+    id = models.AutoField(primary_key=True)
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='spot')
     image=models.ImageField()
     image2=models.ImageField(blank=True,null=True)
     image3=models.ImageField(blank=True,null=True)
     name=models.CharField(max_length=20)
+    short_discription=models.CharField(max_length=50,help_text='short description place in 50 words')
     discription=models.TextField()
+    rel_link=models.URLField(help_text='you can add related articles link about place here',default='https://www.google.com/search?q=kerala&rlz=1C5CHFA_enIN988IN988&oq=ker&aqs=chrome.0.35i39j46i433i512j69i57j69i60l5.1347j0j7&sourceid=chrome&ie=UTF-8')
     link=models.URLField()
     district=models.ForeignKey(districts,on_delete=models.CASCADE)
     type=models.ForeignKey(remark,on_delete=models.CASCADE)
-    warning=models.TextField()
+    warning=models.TextField(help_text='give any spetial advise or any spectial local warning here')
+    key_words=models.TextField(help_text='keyword must start with %,keyword just give your contribution more expo!')
+    date=models.DateTimeField(auto_now_add=True)
     verify=models.BooleanField(default=False)
     
 
     #rating=models.FloatField(default=5)
     def save(self, *args, **kwargs):
+        self.name=self.name.upper()
         new_image = compress(self.image)
         self.image = new_image
         try:
@@ -86,7 +101,8 @@ class spot(models.Model):
 class reviewmodel(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name='reviewmodel')
     content=models.TextField()
-    rating=models.IntegerField(choices=((0, 'Low'),(1, 'Normal'),(2, 'High')),blank=True,null=True)
+    status=models.CharField(max_length=12,choices=(('visited','visited'),('Not visited','Not visited')))
+    rating=models.CharField(max_length=14,choices=(('Below average','Below average'),('Average', 'Average'),('Recomandable', 'Recomandable'),('Good','Good'),('fentastic','fentastic')),blank=True,null=True)
     date=models.DateTimeField(auto_now_add=True)
     spot=models.ForeignKey(spot,on_delete=models.CASCADE,related_name='reviewmodel')
     def __str__(self):
